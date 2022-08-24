1cd4cd49f93db37214caf5599d3f0d31f81fcbb2
mdintezaralam <mintezar99@gmail.com>
2022-08-25 00:38:53 +0530

    third

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import Wave from "../../img/wave.png";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
 import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
 import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import LinkedIn from "../../img/linkedin.png";
 
 const Footer = () => {
   return (
@@ -12,9 +13,15 @@ const Footer = () => {
       <div className="f-content">
         <span>mdintezara35@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
+          <a href="https://www.instagram.com/mdintezar123/">
+            <Insta color="white" size={"3rem"} />
+          </a>
+          <a href="https://www.instagram.com/mdintezar123/">
           <Facebook color="white" size={"3rem"} />
+          </a>
+          <a href="https://github.com/MdIntezarAlam">
           <Gitub color="white" size={"3rem"} />
+          </a>
         </div>
       </div>
     </div>
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-// import boy from "../../img/boy.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
 import FloatinDiv from "../FloatingDiv/FloatingDiv";
